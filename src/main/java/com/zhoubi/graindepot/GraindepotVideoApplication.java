78a76473a74e184445cdfbf337c8fcb2400e1bd1
fredma1987 <356803326@qq.com>
2019-03-20 11:49:13 +0800

    调整-fred

--- src/main/java/com/zhoubi/graindepot/GraindepotVideoApplication.java
+++ src/main/java/com/zhoubi/graindepot/GraindepotVideoApplication.java
@@ -14,7 +14,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan(value = "com.zhoubi.graindepot.mapper")
-@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
+@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE,maxInactiveIntervalInSeconds=4*60*60)
 public class GraindepotVideoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
